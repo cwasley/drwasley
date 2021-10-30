726a6ec03e36919c46c6b9a86c95bc0d5e4935c3
Collin Wasley <collin.wasley@gmail.com>
2021-10-30 16:43:20 -0700

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,27 @@ import Head from 'next/head'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { CssBaseline } from '@material-ui/core'
 import theme from '../src/theme'
+import * as ga from '../lib/ga'
 
 export default function MyApp(props) {
   const { Component, pageProps } = props
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
